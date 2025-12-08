3a2f87b37d7c1f84c9f0929e1f72fb7ce9e12251
nctyler <nctyler@arizona.edu>
2025-12-08 09:13:24 -0500

    Performed Hyperpamater Tuning on Lasso and Gradient Boost Models

--- utils/lasso_regression.py
+++ utils/lasso_regression.py
@@ -1,13 +1,17 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso, LassoCV
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
 
 """
 Contains utility functions for creating, training, and testing
 a lasso regression model.
 """
 
-def lasso_regression(df, y, alpha):
+def lasso_regression(df, y, alpha, random_state):
     # Initializing a variable for the predictor variables
     X = df.drop(y, axis = 1)
 
@@ -15,7 +19,7 @@ def lasso_regression(df, y, alpha):
     y = df[y]
 
     # Splitting the data into training and testing datasets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = random_state)
 
     # Initializing the Lasso Regression Model
     lasso_reg = Lasso(alpha = alpha)
@@ -34,6 +38,47 @@ def lasso_regression(df, y, alpha):
 
     # Initializing a Variable for the MSE and R-Squared Results
     results = f'Mean Squared Error: {mse_lasso}\nR-Squared: {r2_lasso}'
+    
+    # Returning the Results
+    return results
+
+def lasso_hyperparameter_tuning(df, y, cv, max_iter, random_state):
+    '''
+    This function performs hyperparameter tuning on the lasso model.
+    '''
+    # Initializing a variable for the predictor variables
+    X = df.drop(y, axis = 1)
+
+    # Initializing a variable for the response variable
+    y = df[y]
+
+    # Splitting the data into training and testing datasets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = random_state)
+
+    # Defining a range of alpha values for Lasso
+    alphas = np.logspace(-4, 2, 100)
+
+    # Initializing LassoCV
+    lasso_cv = LassoCV(alphas = alphas, cv = cv, max_iter = max_iter, random_state = 42)
+
+    # Fitting the Model
+    lasso_cv.fit(X_train, y_train)
+
+    # Re-Initializing the Model and Fitting it with the Optimal Alpha
+    best_lasso = Lasso(alpha = lasso_cv.alpha_)
+    best_lasso.fit(X_train, y_train)
+
+    # Making New Predictions
+    y_pred_best_lasso = best_lasso.predict(X_test)
+
+    # Calculating the Mean Squared Error
+    mse_best = mean_squared_error(y_test, y_pred_best_lasso)
+
+    # Calculating the R-Swuared Value
+    r2_best = r2_score(y_test, y_pred_best_lasso)
+
+    # Initializing a Variable for the MSE and R-Squared Results
+    results = f'Mean Squared Error: {mse_best}\nR-Squared: {r2_best}'
     
     # Returning the Results
     return results
